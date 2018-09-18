4dce6b4a4c3c7dcf942a3ee5d726c79f6f230f91
Jamie Bayne <jamiebayne@hotmail.com>
2018-09-18 21:16:48 +0100

    Move brightness settings to top of script

--- hooks/brightness.sh
+++ hooks/brightness.sh
@@ -6,7 +6,12 @@ exec >> ~/.redshift-hooks.log 2>&1
 
 # path to github.com/qualiaa/acpi-brightness script
 readonly brightness=$HOME/usr/bin/brightness 
+
 readonly fade_time=$(( 60*60 ))
+readonly day_brightness=90
+readonly transition_brightness=60
+readonly night_brightness=35
+
 
 function brightness() {
     local seconds=${1:-0}
@@ -17,30 +22,36 @@ function brightness() {
 if [[ $1 == period-changed ]]; then
     case $3 in
         daytime)
-            percent=90
+            percent=$day_brightness
             case $2 in
                 transition)
                     brightness $fade_time --inc ;;
                 night|none)
-                    brightness
+                    brightness ;;
+                *)
+                    echo "Unrecognised: $2"
             esac ;;
         transition)
-            percent=60
+            percent=$transition_brightness
             case $2 in
                 daytime)
                     brightness $fade_time --dec ;;
                 night)
                     brightness $fade_time --inc ;;
                 none)
-                    brightness
+                    brightness ;;
+                *)
+                    echo "Unrecognised: $2"
             esac ;;
         night)
-            percent=35
+            percent=$night_brightness
             case $2 in
                 transition)
                     brightness $fade_time --dec ;;
                 day|none)
-                    brightness
+                    brightness ;;
+                *)
+                    echo "Unrecognised: $2"
             esac
     esac
 fi
